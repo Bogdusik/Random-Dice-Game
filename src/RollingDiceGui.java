6d51864ca4d086d4a036968689f8820163e032d0
bogdus1k <166155258+Bogdusik@users.noreply.github.com>
2025-07-23 20:16:37 +0100

    Improve code quality

--- src/RollingDiceGui.java
+++ src/RollingDiceGui.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Random;
 
 public class RollingDiceGui extends JFrame {
-    public RollingDiceGui(){
+
+    private static final int ROLL_TIME_MS = 3000;
+    private static final String RESOURCE_PATH = "resources/";
+
+    public RollingDiceGui() {
         super("Rolling Double Dice");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setPreferredSize(new Dimension(700, 700));
@@ -21,17 +23,17 @@ public class RollingDiceGui extends JFrame {
         JPanel jPanel = new JPanel();
         jPanel.setLayout(null);
 
-        //1.Banner
-        JLabel bannerImg = ImgService.loadImage("resources/banner.png");
+        // 1. Banner
+        JLabel bannerImg = ImgService.loadImage(RESOURCE_PATH + "banner.png");
         bannerImg.setBounds(45,25,600,100);
         jPanel.add(bannerImg);
 
-        //2. Dices
-        JLabel diceOneImg = ImgService.loadImage("resources/dice1.png");
+        // 2. Dice images
+        JLabel diceOneImg = ImgService.loadImage(RESOURCE_PATH + "dice1.png");
         diceOneImg.setBounds(100, 200,200, 200);
         jPanel.add(diceOneImg);
 
-        JLabel diceTwoImg = ImgService.loadImage("resources/dice2.png");
+        JLabel diceTwoImg = ImgService.loadImage(RESOURCE_PATH + "dice2.png");
         diceTwoImg.setBounds(390, 200,200, 200);
         jPanel.add(diceTwoImg);
 
@@ -39,48 +41,33 @@ public class RollingDiceGui extends JFrame {
         Random rand = new Random();
         JButton rollButton = new JButton("Roll");
         rollButton.setBounds(250,550, 200, 50);
-        rollButton.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                rollButton.setEnabled(false);
+        rollButton.addActionListener(e -> {
+            rollButton.setEnabled(false);
 
-                //roll for 3 sec
-                long startTime = System.currentTimeMillis();
-                Thread rollThread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        long endTime = System.currentTimeMillis();
-                        try{
-                            while((endTime - startTime)/100F < 3){
-                                // roll dice
-                                int diceOne = rand.nextInt(1,7);
-                                int diceTwo = rand.nextInt(1,7);
+            long startTime = System.currentTimeMillis();
+            Thread rollThread = new Thread(() -> {
+                try {
+                    while (System.currentTimeMillis() - startTime < ROLL_TIME_MS) {
+                        int diceOne = rand.nextInt(6) + 1;
+                        int diceTwo = rand.nextInt(6) + 1;
 
-                                // update dice Image
-                                ImgService.updateImage(diceOneImg, "resources/dice" + diceOne + ".png");
-                                ImgService.updateImage(diceTwoImg, "resources/dice" + diceTwo + ".png");
+                        ImgService.updateImage(diceOneImg, RESOURCE_PATH + "dice" + diceOne + ".png");
+                        ImgService.updateImage(diceTwoImg, RESOURCE_PATH + "dice" + diceTwo + ".png");
 
+                        repaint();
+                        revalidate();
 
-                                repaint();
-                                revalidate();
-
-                                // sleep thread
-                                Thread.sleep(60);
-
-                                endTime = System.currentTimeMillis();
-                            }
-
-                            rollButton.setEnabled(true);
-                        }
-                        catch (InterruptedException e) {
-                            System.out.println("Threading Error: "+ e);
-                        }
+                        Thread.sleep(60);
                     }
+                } catch (InterruptedException ex) {
+                    System.err.println("Threading Error: " + ex.getMessage());
+                    Thread.currentThread().interrupt();
+                } finally {
+                    rollButton.setEnabled(true);
+                }
+            });
 
-                });
-
-                rollThread.start();
-            }
+            rollThread.start();
         });
         jPanel.add(rollButton);
 
